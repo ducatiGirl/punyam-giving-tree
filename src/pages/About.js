4de6656e2cbf01a2f950ca8ae550ebebe1a43aa4
Anjali Abhilash <anjalyabhilash@gmail.com>
2025-08-30 12:52:16 -0700

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,27 +2,26 @@
 export default function About (){
     return (
         <>
-        <h1 style={{textAlign: "center"}}> Home Page</h1>
-        <h2 style={{color: "darkgreen"}}> What is the Giving Tree? </h2>
+        <h2 style={{color: "darkgreen"}}> What is the Punyam Giving Tree? </h2>
         <ul id = "GivingTreeBulletPoints">
-            <li> The Giving Tree is a community-based charitable initiative designed to bring
-                 joy and support to children and families. It is a visual representation of 
-                 generosity, where a tree holds tags that display the specific wishes of
+            <li> The Punyam Giving Tree is a community-based charitable initiative designed to bring
+                 joy and support to children and families specifically in India. It is a visual representation of 
+                 generosity, where a tree holds mangos that display the specific wishes of
                  children in India who may not otherwise receive gifts. </li>
         </ul>
         <h2 style={{color: "#651111"}}> How it works</h2>
         <ul id = "HowItWorksBulletPoints">
             <li> Children are asked to submit wish lists—often including things like toys, clothing, school supplies, or sometimes basic necessities. </li>
-            <li>A tree is decorated with ornaments.</li>
-            <li>Each ornament contains the child's:</li>
+            <li>A tree is decorated with mangos.</li>
+            <li>Each mango represents a child's:</li>
             <ul id="ornamentContains">
-                <li>First and last name</li>
-                <li>Age</li>
-                <li>Date</li>
+                <li>Name</li>
+                <li>School</li>
+                <li>Needs</li>
                 <li>Wishlist</li>
             </ul>
-            <li>Members of the punyam community select an ornament from the tree</li>
-            <li>They buy the requested gift(s) listed on the ornament, fulfilling that child’s wish</li>
+            <li>Members of the punyam community select an mango from the tree</li>
+            <li>They buy the requested gift(s) listed on the mango, fulfilling that child’s wish</li>
             <li>Gifts are typically returned unwrapped to the organization for sorting and distribution.</li>
             <li>Punyam then gives the child their gift!</li>
         </ul>
