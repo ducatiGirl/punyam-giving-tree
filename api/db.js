0db68c17f469236adc699cc5a0b803bbec123265
Anjali Abhilash <anjalyabhilash@gmail.com>
2025-09-04 20:23:21 -0700

    Update db.js

--- api/db.js
+++ api/db.js
@@ -1,15 +1,8 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('../full_story_of_mango_tree.db');
+const db = new sqlite3.Database('/tmp/needs.db');
 
 db.serialize(() => {
-    // Drop the table if it already exists to ensure a clean slate.
-    // This fixes schema-related errors when columns are changed or added.
-    db.run(`DROP TABLE IF EXISTS mango_stories`);
-    
-    // This CREATE TABLE statement defines the clean and organized schema
-    // that the server.js file will use after it transforms the data
-    // from the Google Sheet's messy column headers.
-    db.run(`CREATE TABLE IF NOT EXISTS mango_stories (
+  db.run(`CREATE TABLE IF NOT EXISTS needs (
       id TEXT PRIMARY KEY,
       schoolName TEXT,
       name TEXT,
