55569a91ff4a8b10606d3a1f82a880d608d7a393
Anjali Abhilash <anjalyabhilash@gmail.com>
2025-09-04 20:50:09 -0700

    Update db.js

--- api/db.js
+++ api/db.js
@@ -1,15 +1,14 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('/tmp/needs.db');
+const db = new sqlite3.Database('/tmp/full-story-for-mango-tree.db');
 
 db.serialize(() => {
-  db.run(`CREATE TABLE IF NOT EXISTS needs (
+  db.run(`CREATE TABLE IF NOT EXISTS full_story_for_mango_tree (
       id TEXT PRIMARY KEY,
       schoolName TEXT,
       name TEXT,
       story TEXT,
-      category TEXT,
-      cost REAL,
       wishlist TEXT,
+      cost REAL,
       sponsored INTEGER
   )`);
 });
